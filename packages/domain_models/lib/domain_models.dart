c0b461c1d21f35741868c7f1cc6c851710637ea9
Pieter van der Westhuizen <pieter@csoft.co.za>
2023-07-25 17:09:30 +0200

    Form with strongly-typed models

---
+++ packages/domain_models/lib/domain_models.dart
@@ -0,0 +1,3 @@
+export 'src/dynamic_form.dart';
+export 'src/dynamic_form_field.dart';
+export 'src/dynamic_form_field_validation.dart';
--- packages/form_repository/lib/src/form_repository.dart
+++ packages/form_repository/lib/src/form_repository.dart
@@ -1,4 +1,70 @@
+import 'dart:convert';
+
+import 'package:domain_models/domain_models.dart';
+
 class FormRepository {
+
+  String getJson() {
+    String json = '''{
+  "id": 1,
+  "name": "Contact Details Form",
+  "fields": [
+    {
+      "type": "TextBox",
+      "label": "Full Name",
+      "key": "fullName",
+      "validations": {
+        "isRequired": true,
+        "minLength": 3,
+        "maxLength": 50
+      }
+    },
+    {
+      "type": "TextBox",
+      "label": "Email",
+      "key": "email",
+      "validations": {
+        "isRequired": true,
+        "email": true
+      }
+    },
+    {
+      "type": "TextBox",
+      "label": "Age",
+      "key": "age",
+      "validations": {
+        "isRequired": false,
+        "numeric": true
+      }
+    },
+    {
+      "type": "DropdownList",
+      "label": "Province",
+      "key": "province",
+      "options": [
+        "Eastern Cape",
+        "Free State",
+        "Gauteng",
+        "KwaZulu-Natal",
+        "Limpopo",
+        "Mpumalanga",
+        "Northern Cape",
+        "North West",
+        "Western Cape"
+      ],
+      "validations": {"isRequired": true}
+    }
+  ]
+}''';
+    return json;
+  }
+
+  Future<DynamicForm> loadForm2() async {
+    //await Future.delayed(const Duration(seconds: 3));
+    final dynamicForm = DynamicForm.fromJson(jsonDecode(getJson()));
+    return dynamicForm;
+  }
+
   Future<List<Map<String, dynamic>>> loadForm({required int id}) async {
     // Imitating calling an API to retrieve the form.
     await Future.delayed(const Duration(seconds: 3));
